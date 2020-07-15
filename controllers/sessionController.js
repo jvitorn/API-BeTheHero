a323f29c5f5252f1913026801308f29608570ed7
jvitorn <joaoeuno2011@gmail.com>
2020-07-15 10:50:20 -0300

    começando a implementação de JWT

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const JWT = require('jsonwebtoken')
 
 class Login {
     async login(user, res) {
@@ -9,9 +10,26 @@ class Login {
         try {
             //find
             const find = await Ong.findOne({ email: email, password: password }, { password: 0 }).sort({ title: 1 }).exec();
+
+
             let msg
             if (find) {
                 msg = 'usuario encontrado'
+
+                const payload = { userID: find._id, userName: find.name, userMail: find.email }
+                const header = {
+                    expiresIn: 300 // expires 
+                }
+                const authenticate = find.nivel == 'user' ? false : true;
+                const SIGNATURE = 'HHTDSI'
+                await JWT.sign(payload, SIGNATURE, header, (err, token) => {
+                    if (err) {
+                        throw new Error('erro');
+                    }
+
+                    res.setHeader('x-access-token', token);
+                    res.status(202).json({ adm: authenticate, auth: true, token: token });
+                })
             }
             // caso concluido irá executar isso
             res.status(202).json({ msg: msg, result: find._id })
