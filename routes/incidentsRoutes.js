6687363f2e152c5e8797913a1d55127c169df2c0
jvitorn <joaoeuno2011@gmail.com>
2020-07-15 12:15:17 -0300

    jwt verificado em algumas requisições

--- routes/incidentsRoutes.js
+++ routes/incidentsRoutes.js
@@ -1,4 +1,5 @@
 const Incident = require('../controllers/incidentsController')
+const { validateToken } = require('../utils/authToken')
 // routes
 const routes = {
     incidents: '/api/incidents',
@@ -11,17 +12,18 @@ module.exports = app => {
         .get((req, res) => {
             Incident.listIncidents(res)
         })
-        .post((req, res) => {
+        .post(validateToken, (req, res) => {
+
             const incident = req.body;
             Incident.createIncidents(incident, res)
         })
     app.route(routes.incidentsId)
-        .put((req, res) => {
+        .put(validateToken, (req, res) => {
             const { id } = req.params;
             const incident = req.body;
             Incident.updateIncidents(id, incident, res)
         })
-        .delete((req, res) => {
+        .delete(validateToken, (req, res) => {
             const { id } = req.params;
             Incident.deleteIncidents(id, res)
         })
